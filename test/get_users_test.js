f22edf18bbf14bca367bab44e24b961a04979187
damar <damar_aji@linkaja.id>
2022-04-11 20:23:40 +0700

    updated

--- test/get_users_test.js
+++ test/get_users_test.js
@@ -6,9 +6,6 @@ const code = require('../helper/response.json');
 const { getUsers } = require('../api/users');
 const schemaUsers = require('../data/schema/get_users_schema.json');
 
-const { executeSql } = require('../seed_data/sql_page');
-const { UPDATE_MOCHAWESOME } = require('../seed_data/query/update');
-
 const page = 2;
 
 const testCase = {
@@ -26,9 +23,6 @@ describe(`@skip @getUsersTest ${testCase.describe}`, () => {
 			const response = await getUsers(page);
 			assert(response.status).to.equal(code.success.ok, response.body.message);
 			assert(response.body).to.be.jsonSchema(schemaUsers);
-		});
-		after(`${testCase.after}`, async() => {
-			await executeSql(UPDATE_MOCHAWESOME);
 		});
 	}); 
 }); 
